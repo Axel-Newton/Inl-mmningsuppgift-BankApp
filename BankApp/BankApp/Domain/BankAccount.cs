f37cb2844bb5c645f9c80648263370ec13667ddd
Axlo137 <axelarsson@icloud.com>
2025-10-16 15:36:34 +0200

    Add transaction handling to BankAccount and new transaction UI

--- BankApp/BankApp/Domain/BankAccount.cs
+++ BankApp/BankApp/Domain/BankAccount.cs
@@ -1,30 +1,60 @@
+using System.ComponentModel;
+using Bankapp.Domain;
+
 namespace BankApp.Domain;
 
 public class BankAccount : IBankAccount
-{
-    public Guid Id { get; private set; } = Guid.NewGuid();
-    public string Name { get; private set; }
-    public AccountType AccountType { get; private set; }
-    public string Currency { get; private set; }
-    public decimal Balance { get; private set; }
-    public DateTime LastUpdated { get; private set; }
-
-    public BankAccount(string name, AccountType accountType, string currency, decimal initialBalance)
-    {
-        Name = name;
-        AccountType = accountType;
-        Currency = currency;
-        Balance = initialBalance;
-        LastUpdated = DateTime.Now;
-    }
     
-    public void Withdraw(decimal amount)
     {
-        Balance -= amount;
-    }
+        public TransactionType TransactionType { get; }
+        public List<Transaction> Transactions => _transactions;
 
-    public void Deposit(decimal amount)
-    {
-        Balance += amount;
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Name { get; private set; }
+        public AccountType AccountType { get; private set; }
+        public string Currency { get; private set; }
+        public decimal Balance { get; private set; }
+        public DateTime LastUpdated { get; private set; }
+
+        private readonly List<Transaction> _transactions = new List<Transaction>();
+        public BankAccount(string name, AccountType accountType, string currency, decimal initialBalance)
+        {
+            Name = name;
+            AccountType = accountType;
+            Currency = currency;
+            Balance = initialBalance;
+            LastUpdated = DateTime.Now;
+        }
+        
+        public void Withdraw(decimal amount, Guid toAccountId, Guid fromAccountId, string description, TransactionType transactionType)
+        {
+            if (amount <= 0) throw new ArgumentException("Withdrawal amount cannot be negative");
+            Balance -= amount;
+            LastUpdated = DateTime.Now;
+            
+            _transactions.Add(new Transaction(
+                fromAccountId,
+                toAccountId,
+                amount,
+                DateTime.Now,
+                description,
+                TransactionType.Withdraw
+            ));
+        }
+
+        public void Deposit(decimal amount, Guid toAccountId, Guid fromAccountId, string description, TransactionType transactionType)
+        {
+            if (amount <= 0) throw new ArgumentException("Deposit amount cannot be negative");
+            Balance += amount;
+            LastUpdated = DateTime.Now;
+            
+            _transactions.Add(new Transaction(
+                fromAccountId,
+                toAccountId,
+                amount,
+                DateTime.Now,
+                description,
+                TransactionType.Deposit
+            ));
+        }
     }
-}
---
+++ BankApp/BankApp/Domain/Transaction.cs
@@ -0,0 +1,32 @@
+﻿namespace Bankapp.Domain
+{
+    public enum TransactionType
+    {
+        Deposit,
+        Withdraw,
+        TransferIn,
+        TransferOut
+    }
+    
+    public class Transaction
+    {
+        public Guid Id { get; set; } =Guid.NewGuid();
+        public Guid? FromAccountId { get; set; }
+        public Guid? ToAccountId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public TransactionType TransactionType { get; set; } 
+        public string Description { get; set; }
+
+        public Transaction(Guid fromAccountId, Guid toAccountId, decimal amount, DateTime timeStamp, string description, TransactionType transactionType)
+        {
+            FromAccountId = fromAccountId;
+            ToAccountId = toAccountId;
+            Amount = amount;
+            TimeStamp = DateTime.Now;
+            Description = description;
+            TransactionType = transactionType;
+        }
+    }
+}
+
