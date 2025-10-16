2928fea28f29a588ace18e3353e491c7b198a849
Axel-Newton <axel.larsson@edu.newton.se>
2025-10-16 10:57:32 +0200

    added comments

--- BankApp/BankApp/Services/LocalStorageService.cs
+++ BankApp/BankApp/Services/LocalStorageService.cs
@@ -6,18 +6,18 @@ public class LocalStorageService : IStorageService
     private readonly IJSRuntime _js;
     private const string StorageKey = "accounts";
 
-    public LocalStorageService(IJSRuntime js)
+    public LocalStorageService(IJSRuntime js) //Constructor for LocalStorageService class
     {
         _js = js;
     }
 
-    public async Task SaveAccountsAsync(List<IBankAccount> accounts)
+    public async Task SaveAccountsAsync(List<IBankAccount> accounts) //Saves accounts to local storage
     {
         var json = JsonSerializer.Serialize(accounts);
         await _js.InvokeVoidAsync("localStorage.setItem", StorageKey, json);
     }
 
-    public async Task<List<IBankAccount>> LoadAccountsAsync()
+    public async Task<List<IBankAccount>> LoadAccountsAsync() //method that retrieves list of bank accounts stored in browser
     {
         var json = await _js.InvokeAsync<string>("localStorage.getItem", StorageKey);
         return json is null
@@ -25,7 +25,7 @@ public class LocalStorageService : IStorageService
             : JsonSerializer.Deserialize<List<IBankAccount>>(json) ?? new List<IBankAccount>();
     }
 
-    public async Task DeleteAccount(Guid accountId)
+    public async Task DeleteAccount(Guid accountId) //method that deletes a specific bank account
     {
         var accounts = await LoadAccountsAsync();
         var updatedAccounts = accounts.Where(account => account.Id != accountId).ToList();
